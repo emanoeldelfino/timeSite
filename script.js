a45199c058e96aa7852d69a106b7ef9f403c222e
dlfno <emanoeldlfno@gmail.com>
2021-03-23 01:22:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function zeroPad(number, width) {
+    let string = String(number);
+    while (string.length < width) {
+      string = "0" + string;
+    }
+    return string;
+  }
+
 var date = new Date();
 var hour = date.getHours();
 var minutes = date.getMinutes();
@@ -11,18 +19,18 @@ var partDay, color;
 
 if (hour < 0 || hour > 23) {
     alert('Invalid hour.');
-} else if (hour > 4 && hour < 12) {
-    partDay = 'morning';
-    colorCode = '46b3d3';
-} else if (hour < 19) {
+} else if (hour >= 19 || hour < 5) {
+    partDay = 'night';
+    colorCode = '081b1f';
+} else if (hour > 12) {
     partDay = 'afternoon';
     colorCode = 'df5946';
 } else {
-    partDay = 'night';
-    colorCode = '081b1f';
+    partDay = 'morning';
+    colorCode = '46b3d3';
 }
 
-timeP.innerHTML = `Now is <strong>${hour}:${minutes}</strong>.`;
+timeP.innerHTML = `Now is <strong>${zeroPad(hour, 2)}:${zeroPad(minutes, 2)}</strong>.`;
 greetP.innerHTML = `Good <strong>${partDay}</strong>!`;
 timeImg.src = `images/${partDay}.png`;
 body.style.background = `#${colorCode}`;
